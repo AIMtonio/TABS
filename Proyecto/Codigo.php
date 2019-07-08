068b3ef417ab83bbc5c403f948e33c5e0a580968
AIMtonio <36789102+AIMtonio@users.noreply.github.com>
2019-07-07 21:40:20 -0500

    codigo

--- Proyecto/Codigo.php
+++ Proyecto/Codigo.php
@@ -1,7 +1,18 @@
 <?php
 session_start();
-$db=mysqli_connect('remotemysql.com','Rgpi7ESSmM','mAqyxMUQZa','Rgpi7ESSmM');
+$db=mysqli_connect('localhost','root','','agencia');
+$categoria=0;
 
 $categoria=mysqli_query($db,"SELECT * FROM categorias WHERE id_categoria='cat1'");
+//$_SESSION['mensaje']=$validarAlerta;
+$row=mysqli_fetch_array($categoria);
+    if(is_null($row)){
+        $_SESSION['mensaje']='<script name="accion">alert("no existe") </script>';
+}else{
+	$_SESSION['mensaje']='<script name="accion">alert("si existe uno") </script>';
+}
+
+
+    
 
 ?>
--- Proyecto/index.php
+++ Proyecto/index.php
@@ -16,15 +16,45 @@
 </center>
 
 <nav>
-	<a href="#"><?php $categoria ?></a>
-	<a href="#">categoria2</a>
+	<a href="#">categoria</a>
+	<a href="#">categoria</a>
 	<a href="#">categoria</a>
 	<a href="#">categoria</a>
 	<a href="#">categoria</a>
 	<div class="animation start-home"></div>
 </nav>
+<div><?php $categoria ?></div>
+<div class="mensaje">
+    <?php
+    $mensaje=$_SESSION['mensaje']; 
+        echo $mensaje; 
+?>
+</div>
 
 
+<?php $results=mysqli_query($db,"SELECT*FROM categorias");
+    $row=mysqli_fetch_array($results);
+    if(is_null($row)){
+        ?> <div class="vacio"> Sin Registros
+    </div> <?php
+    }else{
+        ?>
+    <table>
+    <thead>
+        <tr>
+        <th>Nombre</th>
+            <th>Descripcion categoria</th>
+        </tr>
+        </thead>
+        <?php $results=mysqli_query($db,"SELECT*FROM categorias");
+        while($row = mysqli_fetch_array($results)){?>
+        <tr>
+        <td><?php echo $row['nombre']; ?></td>
+        <td><?php echo $row['descripcion']; ?></td>
+            
+        </tr>
+      <?php } }?>
+    </table>
 
 </body>
 </html>
